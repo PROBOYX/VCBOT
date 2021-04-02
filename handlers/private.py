c10c1ad92ff754c314ff252dc7464345f8bc13f5
PROBOY X <68194873+PROBOYX@users.noreply.github.com>
2021-04-02 10:36:40 +0530

    Update private.py

--- handlers/private.py
+++ handlers/private.py
@@ -7,7 +7,7 @@ from helpers.filters import other_filters2
 @Client.on_message(other_filters2)
 async def start(_, message: Message):
     await message.reply_text(
-        f"""I am an open-source @CallsMusic bot, I let you play music in your group's voice chat.
+        f"""I am an  LEGENDX bot, I let you play music in your group's voice chat.
 
 The commands I currently support are:
 
@@ -21,10 +21,10 @@ The commands I currently support are:
             [
                 [
                     InlineKeyboardButton(
-                        "Group", url="https://t.me/callsmusicchat"
+                        "Group", url="https://t.me/ultraxchat"
                     ),
                     InlineKeyboardButton(
-                        "Channel", url="https://t.me/callsmusic"
+                        "Channel", url="https://t.me/ultraxot"
                     )
                 ]
             ]
